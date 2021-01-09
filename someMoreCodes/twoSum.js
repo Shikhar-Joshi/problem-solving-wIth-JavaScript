34f4204a534138b9027c25a3853201fd07db40be
Shikhar-Joshi <shikharjoshi03@gmail.com>
2021-01-09 09:39:36 +0530

    Some optimization

--- someMoreCodes/twoSum.js
+++ someMoreCodes/twoSum.js
@@ -5,18 +5,18 @@ const twoSum = (arr, k) =>{
     let resultArray = [];
     for(let num of arr){
         if(remElem.includes(num)){
-            resultArray.push(Math.abs(k - num));
+            resultArray.push(k - num);
             resultArray.push(num);
             return resultArray;
         }
         else{
-            remElem.push(Math.abs(k-num));
+            remElem.push(k-num);
         }
     }
 };
 
-let arr = [2,7,11,15];
-let k=9;
+let arr = [2,11,15,7, -1];
+let k=10;
 
 let remElem = twoSum(arr, k);
 console.log(remElem);
