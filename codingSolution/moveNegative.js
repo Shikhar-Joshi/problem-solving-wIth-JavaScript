157f93ab974677caf0243a57815f7f14f2ad4e97
Shikhar-Joshi <shikharjoshi03@gmail.com>
2021-02-02 23:12:56 +0530

    Move negative numbers in the beginning

--- codingSolution/moveNegative.js
+++ codingSolution/moveNegative.js
@@ -1,11 +1,4 @@
 // Question Link: https://www.geeksforgeeks.org/move-negative-numbers-beginning-positive-end-constant-extra-space/
-const swap = (num1, num2) =>{
-	num1 = num1^num2;
-	num2 = num1^num2;
-	num1 = num1^num2;
-	
-	return [num1, num2];
-};
 
 const moveNegative = (arr) =>{
     const pivot = 0;
@@ -13,7 +6,7 @@ const moveNegative = (arr) =>{
     for(let j=0;j<len;j++){
         if(arr[j] < pivot){
             i++;
-            [arr[i], arr[j]] = swap(arr[i], arr[j]);
+            [arr[i], arr[j]] = [arr[j], arr[i]];
         }
     }
     return arr;
