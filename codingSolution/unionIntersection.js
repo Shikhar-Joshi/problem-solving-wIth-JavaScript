16d98a597aeee8cd02f06ce1e026410387ab8fb5
Shikhar-Joshi <shikharjoshi03@gmail.com>
2021-02-06 10:50:21 +0530

    Union and Intersection of two arrays

--- codingSolution/unionIntersection.js
+++ codingSolution/unionIntersection.js
@@ -1,17 +1,18 @@
 // Question Link: https://practice.geeksforgeeks.org/problems/union-of-two-arrays/0
-class Operation{
-    union(arr1, arr2) {
-        let union = [...arr1, ...arr2];
-        return union;
-    }
-    intersection(arr1, arr2) {
-        let intersection = arr1.filter(x => arr2.includes(x));
-        return intersection;
-    }
+
+const lodash = require("lodash");
+
+function unionHandler(arr1, arr2) {
+    let union = [...arr1, ...arr2];
+    return union;
+}
+function intersectionHandler(arr1, arr2) {
+    let intersectArrray = lodash.intersection(arr1, arr2);
+    return intersectArrray;
 }
 
-let arr1 = [1,3,4,5];
+let arr1 = [10,2,45,5];
 let arr2 = [1,2,5,6,7];
 
-const oper = new Operation();
-console.log(oper.intersection(arr1, arr2));
+console.log(intersectionHandler(arr1, arr2));
+console.log(unionHandler(arr1, arr2));
