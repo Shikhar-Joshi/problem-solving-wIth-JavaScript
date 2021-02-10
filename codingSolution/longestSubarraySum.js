62e97e49ef0b2070d6a559b44e67316b903236e8
Shikhar-Joshi <shikharjoshi03@gmail.com>
2021-02-10 15:20:31 +0530

    Program to find largest contiguous array sum

--- codingSolution/longestSubarraySum.js
+++ codingSolution/longestSubarraySum.js
@@ -1,9 +1,13 @@
-function sumSubArray(arr) {
-	let max_sum = Math.max(...arr);
+// Question Link: https://www.geeksforgeeks.org/largest-sum-contiguous-subarray/
 
+function sumSubArray(arr, len) {
+	
+	//in case if there is only negative numbers in the array
+	let max_sum = Math.max(...arr);
 	if(max_sum < 0) {
 		return max_sum;
 	}
+	
 	max_sum = 0;
 	const _ = arr.reduce((acc, value) =>{
 		acc = acc + value > 0? acc + value :0;
@@ -14,6 +18,6 @@ function sumSubArray(arr) {
 }
 
 
-const arr = [-8, -3, -6, -2, -5, -4];
-console.log(sumSubArray(arr));
- 
+const arr = [-2, -3, 4, -1, -2, 1, 5, -3];
+let len = arr.length;
+console.log(sumSubArray(arr, len));
