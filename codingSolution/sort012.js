ce07aae654f96e13159e42cdf412f02672b4676f
Shikhar-Joshi <shikharjoshi03@gmail.com>
2021-02-10 15:27:22 +0530

    Sort 0s, 1s & 2s withour using any sorting algo

--- codingSolution/sort012.js
+++ codingSolution/sort012.js
@@ -1,11 +1,4 @@
 // Question Link: https://www.geeksforgeeks.org/sort-an-array-of-0s-1s-and-2s/
-const swap = (num1, num2) =>{
-	num1 = num1^num2;
-	num2 = num1^num2;
-	num1 = num1^num2;
-	
-	return [num1, num2];
-};
 
 let arr = [0,0,1,1,1,1,1,2,0,0,2];
 let low = 0, mid = 0;
@@ -14,7 +7,7 @@ let high = arr.length - 1;
 while(mid <= high){
 	switch(arr[mid]){
 		case 0:
-			[arr[mid], arr[low]] = swap(arr[mid], arr[low]);
+			[arr[mid], arr[low]] = [arr[low], arr[mid]];
 			mid++; low++;
 			break;
 		case 1:
@@ -22,7 +15,7 @@ while(mid <= high){
 			break;
 		
 		case 2:
-			[arr[mid], arr[high]] = swap(arr[mid], arr[high]);
+			[arr[mid], arr[high]] = [arr[high], arr[mid]];
 			high--;
 			break;
 	}
